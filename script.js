d7629aef38f2047b3d2e075a4c765e2d03ab5ea6
Bruno Gonçalves <brunogon463@gmail.com>
2024-04-07 23:19:36 -0300

    updates

---
+++ script.js
@@ -0,0 +1,40 @@
+// MODAL
+
+const modal = document.querySelector('.modal')
+const botaoAbrirModal = document.querySelector('.icon-eng')
+const botaofecharmodal = document.querySelector('.botao-fechar-modal');
+const botaofecharmodall = document.querySelector('.footer-modal');
+
+
+botaoAbrirModal.addEventListener('click', () => {
+    modal.showModal();
+})     
+
+botaofecharmodal.addEventListener('click', () => {
+    modal.close();
+
+})
+
+botaofecharmodall.addEventListener('click', () => {
+    modal.close()
+})
+
+
+// DARK OR LIGHT
+
+const botao = document.querySelector('.botao');
+const body = document.body;
+const elemento = document.querySelector('.txt-animado'); 
+const icons = document.querySelector('.logos');
+const baixar = document.querySelector('#botaodownload');
+const downl = document.querySelector('.botaobaixar');
+botao.onclick = function() {
+    this.classList.toggle('active');
+    modal.classList.toggle('active');
+    body.classList.toggle('active');
+    elemento.classList.toggle('active');
+    icons.classList.toggle('active');
+    downl.classList.toggle('active');
+    baixar.classList.toggle('active');
+}
+
